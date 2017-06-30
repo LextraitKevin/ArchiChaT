b47c49e95b65fceec606e74e35dd897cce5b24e8
Kevin Lextrait <kevinlextrait@gmail.com>
2017-06-30 20:22:54 +0200

    add post msg for client

--- src/ArchiChaT/Client.java
+++ src/ArchiChaT/Client.java
@@ -1,5 +1,6 @@
 package ArchiChaT;
 
+import ArchiChaT.Models.Message;
 import ArchiChaT.Models.User;
 import ArchiChaT.Rest.RestMessage;
 import ArchiChaT.Rest.RestUser;
@@ -15,6 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import ArchiChaT.Services.AuthService;
 import ArchiChaT.Services.IAuthService;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -40,60 +42,145 @@ public class Client {
 
 		String pseudo ="";
 		String password ="";
+		User currentUser= null;
 
 
 
 		while (life) {
 
 			System.out.println("Saisissez une action : ");
-			System.out.println("ca : modifier son compte \n" +
-					"quit : quitter l'application \n"+
-					"connexion : connexion au compte");
+
+			if (connected == false) {
+
+
+				System.out.println("quit : quitter l'application \n" +
+						"connexion : connexion au compte");
+			}else{
+				System.out.println("update : modifier son compte \n"+
+						"addFriend : Ajouter un ami pour discuter\n"+
+						"onlineFriend : voir mes amis connectes\n"+
+						"sendMsg : Envoyer un message à un ami"
+				);
+			}
 
 			String action = sc.nextLine();
 
+
 			switch (action) {
 
-				case "ca":
-					System.out.println("Saisissez un pseudo : ");
-					pseudo = sc.nextLine();
+				case "sendMsg":
 
-					System.out.println("Saisissez un mot de passe : ");
-					password = sc.nextLine();
+					if(connected) {
 
-					User user = new User(1, pseudo, password);
+						System.out.println("Saisissez l'identifiant de votre ami : ");
+						String id = sc.nextLine();
 
-					String json = gson.toJson(user);
+						System.out.println("Saisissez votre message : ");
+						String contentMSG = sc.nextLine();
 
-					System.out.println(httpRequest("http://localhost:8080/ArchiChaT_Web_exploded/users/"+user.getId(), "PUT", json));
+						String content = sendGet("http://localhost:8080/ArchiChaT_Web_exploded/users/"+ id);
+
+						User destinataire = new Gson().fromJson(content,User.class);
+
+						Message msg = new Message(1,currentUser,contentMSG,destinataire);
+
+						String json = gson.toJson(msg);
+
+
+						System.out.println(httpRequest("http://localhost:8080/ArchiChaT_Web_exploded/messages/", "POST", json));
+
+					}else{
+						System.out.println("You must be connected to do this action");
+					}
+
+
+					break;
+
+
+				case "addFriend" :
+
+					if(connected) {
+
+
+						System.out.println("Saisissez l'identifiant de votre ami : ");
+
+
+						String idFriend = sc.nextLine();
+
+						ArrayList<User> oldFriend = currentUser.getFriends();
+
+						String content = sendGet("http://localhost:8080/ArchiChaT_Web_exploded/users/"+ idFriend);
+
+						User friend = new Gson().fromJson(content,User.class);
+
+						oldFriend.add(friend);
+
+						currentUser.setFriends(oldFriend);
+
+						String json = gson.toJson(currentUser);
+
+						System.out.println(httpRequest("http://localhost:8080/ArchiChaT_Web_exploded/users/" + currentUser.getId(), "PUT", json));
+					}else{
+						System.out.println("You must be connected to do this action");
+					}
+
+					break;
+
+				case "update":
+					if(connected) {
+						System.out.println("Saisissez un pseudo : ");
+						pseudo = sc.nextLine();
+
+						System.out.println("Saisissez un mot de passe : ");
+						password = sc.nextLine();
+
+						User user = new User(1, pseudo, password);
+
+						String json = gson.toJson(user);
+
+						System.out.println(httpRequest("http://localhost:8080/ArchiChaT_Web_exploded/users/" + user.getId(), "PUT", json));
+					}else{
+						System.out.println("You must be connected to do this action");
+					}
 
 					break;
 
 				case "connexion" :
-					System.out.println("Saisissez un pseudo : ");
-					pseudo = sc.nextLine();
 
-					System.out.println("Saisissez un mot de passe : ");
-					password = sc.nextLine();
+					if(connected) {
 
-					String content = sendGet("http://localhost:8080/ArchiChaT_Web_exploded/users");
+						System.out.println("You are already connected");
 
-					Type listType = new TypeToken<ArrayList<User>>(){}.getType();
+					}else {
+						System.out.println("Saisissez un pseudo : ");
+						pseudo = sc.nextLine();
 
-					List<User> yourClassList = new Gson().fromJson(content, listType);
+						System.out.println("Saisissez un mot de passe : ");
+						password = sc.nextLine();
 
-					for (User u: yourClassList) {
+						String content = sendGet("http://localhost:8080/ArchiChaT_Web_exploded/users");
 
-						if(u.getPassword().equals(password) && u.getName().equals(pseudo)){
-							connected=true;
+						Type listType = new TypeToken<ArrayList<User>>() {
+						}.getType();
+
+						List<User> yourClassList = new Gson().fromJson(content, listType);
+
+						for (User u : yourClassList) {
+
+							if (u.getPassword().equals(password) && u.getName().equals(pseudo)) {
+								currentUser=u;
+								connected = true;
+
+							}
 						}
-					}
 
-					System.out.println(connected);
+						System.out.println(connected);
+					}
 
 					break;
 
 				case "quit":
+					System.out.println("See you soon my friend");
 					life=false;
 					break;
 
